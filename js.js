5cfaa1aadc0bad2bae045559d1cf8081d07f71fd
Chris Sidarous <sidarousca@gmail.com>
2023-08-10 21:24:37 -0400

    Update

--- js.js
+++ js.js
@@ -35,3 +35,67 @@ function PageTransitions(){
 }
 
 PageTransitions();
+
+
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+
+
+
+/*
+  document.addEventListener('DOMContentLoaded', () => {
+    const posts = document.querySelectorAll('.post');
+
+    posts.forEach(post => {
+        const readMoreBtn = post.querySelector('.read-more-btn');
+        const postContent = post.querySelector('.post-content');
+        const postContentInnertext = postContent.innerText;
+        console.log(postContentInnertext);
+        readMoreBtn.addEventListener('click', () => {
+            postContent.classList.toggle('show');
+            
+            readMoreBtn.innerText = postContent.classList.contains('show') ? 'Read Less' : 'Read More';
+
+            if(readMoreBtn.innerText == 'Read Less'){
+                postContent.innerText = " ";
+            }
+            else{
+                postContent.innerText= postContentInnertext;
+            }
+        });
+    });
+});
+
+<section class="section sec5 interests " id= "interests" > 
+            <h1> Blog Posts</h1>
+              <section class="post">
+                <h2>My Summer Garden Experience</h2>
+                <div class="post-content">
+                  <p>I planted...</p>
+              </div>
+                <button class="read-more-btn">Read More</button>
+              </section>
+    
+            <section class="post">
+                <h2>How was my Summer 2023 Job? </h2>
+                <div class="post-content">
+                <p>Lavner</p>
+              </div>
+              <button class="read-more-btn">Read More</button>
+            </section>
+        </section>
+
+        <div class= "control control-5 " title ="Blog Posts" data-id= "interests">
+                <i class="fa fa-pen"></i>
+            </div>
+        */
